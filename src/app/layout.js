211da776d596a2550e1b5da1888e3371f751315a
luvlyloozer143 <luvlyloozer143@gmail.com>
2025-11-13 08:07:24 +0530

    Fix: corrected globals.css import path in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import SessionWrapper from "@/components/SessionWrapper";
 import LeftSidebar from "@/components/layout/LeftSidebar";
